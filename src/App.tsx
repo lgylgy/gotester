510163ce6e019bc1fd6b41674ef2cd25a88e78d3
Loic Geffroy <loic.geffroy@masagroup.net>
2021-01-24 18:06:04 +0000

    cleaning generated code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
-import logo from './logo.svg'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 import './App.css'
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Learn React
-                </a>
+                <Container fixed>
+                    <Typography
+                        component="div"
+                        style={{ backgroundColor: '#cfe8fc', height: '100vh' }}
+                    />
+                </Container>
             </header>
         </div>
     )
